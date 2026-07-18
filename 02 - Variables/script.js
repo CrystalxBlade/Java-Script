d1d8d0627292e1407a6b4df3972257553bf1519a
CrystalxBlade <satish141m@gmail.com>
2026-07-18 21:39:10 +0530

    Variable

---
+++ 02 - Variables/script.js
@@ -0,0 +1,35 @@
+// let
+let name = "Blade";
+console.log("Name:", name);
+
+// Value can be changed
+name = "Crystal";
+console.log("Name:", name);
+
+// ====================================
+
+// const
+const country = "India";
+console.log("Country:", country);
+
+
+// country = "Spain"; ❌ Error
+// const variables cannot be reassigned
+
+
+// ====================================
+
+// var
+
+var age = 24;
+console.log("age", age);
+
+age = 25;
+console.log("age:", age);
+
+// ====================================
+
+// Undefined Variable
+
+let city;
+console.log(city);
