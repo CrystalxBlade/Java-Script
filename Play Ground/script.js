159c451928afb99ff772c081a82ce8fd967fde98
CrystalxBlade <satish141m@gmail.com>
2026-08-22 17:33:44 +0530

    Playgroung

---
+++ Play Ground/script.js
@@ -0,0 +1,17 @@
+
+
+let amt = 750;
+
+let people = 4;
+
+let split = (amt/people);
+
+document.writeln("<h3> Split: </h3>"+split + "<br>");
+
+let principle = 5927; 
+let interest = 8;
+let time = 2;
+
+let simpleInterest = (principle * interest * time) / 100;
+
+document.write("<h3> simpleInterest: </h3>"+simpleInterest);
