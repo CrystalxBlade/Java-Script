82617bad80882acf63aa7e86e098e193fb912b80
CrystalxBlade <satish141m@gmail.com>
2026-08-25 12:07:40 +0530

    mixedarray

---
+++ 08 - Arrays/Arrays.js
@@ -0,0 +1,11 @@
+
+let mnixedArray = [2, "Banana", null, undefined, true, 'Z']; 
+
+for(let mixed of mnixedArray)
+{
+    console.log(mixed);
+}
+
+let number = new Array(10, 20, 30, 40);
+
+console.log(number);
