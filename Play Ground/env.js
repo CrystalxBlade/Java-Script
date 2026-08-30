f5eebb9255510596bd125151a45c3a35493345ee
CrystalxBlade <satish141m@gmail.com>
2026-08-30 22:25:56 +0530

    element

--- Play Ground/env.js
+++ Play Ground/env.js
@@ -5,12 +5,6 @@ let fruit = ["Apple", "Mango", "Banana", "Kiwi"];
 let vegies = ["Potato", "Tomato", "Cucumber", "Carrot"];
 
 
-// fruit.pop(); 
-// fruit.push("Grapes");
-
-fruit.shift();
-// fruit.unshift("Grapes");
-console.log();
 
 for(let fruits of fruit)
 {
