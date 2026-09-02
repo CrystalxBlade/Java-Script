bbcf82f1f20a2f299a3af51840a12858d99f40e3
CrystalxBlade <satish141m@gmail.com>
2026-09-02 13:33:11 +0530

    filter

--- Play Ground/env.js
+++ Play Ground/env.js
@@ -1,8 +1,10 @@
 
 
-let num = [5, 2, 7, 9];
+let num = [5, 2, 7, 9, 6, 22, 15, 18, 55, 69, 70];
 
-num.forEach((val) => 
+let arr = num.filter((val) => 
     {
-        console.log(val * val);
+        return val%2 != 0;
     })
+
+console.log(arr);
