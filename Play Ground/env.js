55462ae29dbc2f02d9e8133823a3cd66be4e5528
CrystalxBlade <satish141m@gmail.com>
2026-09-17 20:40:13 +0530

    attributes

--- Play Ground/env.js
+++ Play Ground/env.js
@@ -3,6 +3,14 @@
 let box  = document.querySelector("div");
 console.dir(box);
 
-let heading = document.querySelector("h1");
+let h = document.querySelector("h1");
+
+let at = h.getAttribute("id");
+console.dir(at);
 
 let crystal = document.querySelector("h2");
+
+let prop = document.querySelector("p")
+console.dir(prop);
+
+console.log(prop.getAttribute("class"));
