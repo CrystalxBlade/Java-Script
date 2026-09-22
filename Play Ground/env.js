ec4363cc92c666b18964504263bcafa3e39576ea
CrystalxBlade <satish141m@gmail.com>
2026-09-22 18:50:02 +0530

    object

--- Play Ground/env.js
+++ Play Ground/env.js
@@ -1,8 +1,11 @@
 let btn1 = document.querySelector("#first");
 
-btn1.onclick = (e) =>
+btn1.onclick = (evt) =>
 {
-    console.log(e);
+    console.log(evt);
+    console.log(evt.type)
+    console.log(evt.clientX, evt.clientY)
+    console.log(evt.target)
 }
 
 let box = document.querySelector("div");
